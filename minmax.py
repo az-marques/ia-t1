6f5b8892d98a21168d12bd91bb9e4fe94659068c
Mariana Pillon <marianalopespillon@gmail.com>
2025-05-10 17:24:43 -0300

    created files

---
+++ minmax.py
@@ -0,0 +1,6 @@
+# s0 is empty board - max node
+# possible moves are [0-4] unless column is full
+# funcao de avaliacao:
+# (a * n° linhas abertas) +
+# (b * n° de trincas) +
+# (c * n° de peças no centro)
