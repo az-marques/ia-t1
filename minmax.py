1eedb0c66804c4649de6361492f07eea7e873494
az-marques <85958771+az-marques@users.noreply.github.com>
2025-05-14 17:30:29 -0300

    eval with customizable weights

--- minmax.py
+++ minmax.py
@@ -7,6 +7,15 @@
 from state import State
 import random
 
+eval_values = [1, #p1_longest_line weight
+               1, #p1_avg_line_len weight
+               1, #p1_n_lines weight
+               1, #p1_center_pieces weight
+               -1, #p2_longest_line weight
+               -1, #p2_avg_line_len weight
+               -1, #p2_n_lines weight
+               -1,] #p2_center_pieces weight
+
 #for testing
 def random_move(state: State):
     return random.choice(state.legal_moves())
@@ -56,9 +65,30 @@ def is_cutoff(state: State, depth):
         return True
     return False
 
-#PLACEHOLDER eval function
 def eval(state: State):
-    return max([state.longest_h_line(state.p1_char),
-         state.longest_v_line(state.p1_char),
-         state.longest_d_up_line(state.p1_char),
-         state.longest_d_down_line(state.p1_char),])
+    p1_longest_line = max([state.longest_h_line(state.p1_char),
+                           state.longest_v_line(state.p1_char),
+                           state.longest_d_up_line(state.p1_char),
+                           state.longest_d_down_line(state.p1_char),])
+    p1_longest_line = min(p1_longest_line, 4) #no benefit to line longer than 4 - a game ending with a 5 line is not a "better" win
+    p1_n_lines, p1_avg_line_len = state.line_stats(state.p1_char)
+    p1_center_pieces = state.center_pieces(state.p1_char)
+
+    p2_longest_line = max([state.longest_h_line(state.p2_char),
+                           state.longest_v_line(state.p2_char),
+                           state.longest_d_up_line(state.p2_char),
+                           state.longest_d_down_line(state.p2_char),])
+    p2_longest_line = min(p2_longest_line, 4) #no benefit to line longer than 4 - a game ending with a 5 line is not a "better" win
+    p2_n_lines, p2_avg_line_len = state.line_stats(state.p2_char)
+    p2_center_pieces = state.center_pieces(state.p2_char)
+
+    return (eval_values[0]*p1_longest_line +
+            eval_values[1]*p1_avg_line_len +
+            eval_values[2]*p1_n_lines + 
+            eval_values[3]*p1_center_pieces +
+
+            eval_values[4]*p2_longest_line +
+            eval_values[5]*p2_avg_line_len +
+            eval_values[6]*p2_n_lines +
+            eval_values[7]*p2_center_pieces)
+    
